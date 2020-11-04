c8bebb790df3aa8b5aaabc572badb23e1611db51
Ron B <me@ronthecookie.me>
2020-11-04 23:29:41 +0200

    add rainbow ezsummon

--- datapack/voicelog.js
+++ datapack/voicelog.js
@@ -3,10 +3,75 @@ const fs = require("fs");
 const sounds = require("../resourcepack/assets/arc86/sounds.json");
 const tickLines = [];
 const psLines = [];
+const ezSummon = [{ text: "\n" }];
+const rainbow = [
+	"e87d7d",
+	"e88a7d",
+	"e8977d",
+	"e8a47d",
+	"e8b07d",
+	"e8bd7d",
+	"e8ca7d",
+	"e8d77d",
+	"e8e47d",
+	"dfe87d",
+	"d3e87d",
+	"c6e87d",
+	"b9e87d",
+	"ace87d",
+	"9fe87d",
+	"92e87d",
+	"86e87d",
+	"7de881",
+	"7de88e",
+	"7de89b",
+	"7de8a8",
+	"7de8b5",
+	"7de8c1",
+	"7de8ce",
+	"7de8db",
+	"7de8e8",
+	"7ddbe8",
+	"7dcee8",
+	"7dc1e8",
+	"7db5e8",
+	"7da8e8",
+	"7d9be8",
+	"7d8ee8",
+	"7d81e8",
+	"867de8",
+	"927de8",
+	"9f7de8",
+	"ac7de8",
+	"b97de8",
+	"c67de8",
+	"d37de8",
+	"df7de8",
+	"e87de4",
+	"e87dd7",
+	"e87dca",
+	"e87dbd",
+	"e87db0",
+	"e87da4",
+	"e87d97",
+	"e87d8a",
+	"e87d7d",
+];
 
 Object.keys(sounds).map(key => {
 	const { id, endTicks } = sounds[key];
 	if (!id || !endTicks) return;
+
+	ezSummon.push({
+		text: `${key} (#${id}) `,
+		bold: true,
+		color: `#${rainbow[(id % rainbow.length)*2]}`,
+		clickEvent: {
+			action: "run_command",
+			value: `/scoreboard players set @e[tag=voicelog,limit=1,sort=nearest,distance=..10] voicelogID ${id}`,
+		},
+	});
+
 	tickLines.push(
 		`execute if score @s currentVoicelog matches ${id} if score @s voicelogTime matches ${endTicks}.. run function arc86:voicelogs/ps_over`
 	);
@@ -51,5 +116,20 @@ fs.writeFileSync(
 		"genpstick.mcfunction"
 	),
 	tickOut,
+	{ encoding: "utf8" }
+);
+fs.writeFileSync(
+	require("path").join(
+		__dirname,
+		"data",
+		"arc86",
+		"functions",
+		"voicelogs",
+		"genezsummon.mcfunction"
+	),
+	`# blah blah voicelog.js made this you get the point
+setblock ~ ~ ~ air
+function arc86:voicelogs/summon
+tellraw @a[distance=..7] ${JSON.stringify(ezSummon)}`,
 	{ encoding: "utf8" }
 );
