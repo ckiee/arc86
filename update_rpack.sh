f7e8a345b6e4a78631c593ab33c9624214a2dbec
ckie <me@ronthecookie.me>
2021-06-18 00:35:26 +0300

    bump, update hostname

--- update_rpack.sh
+++ update_rpack.sh
@@ -9,6 +9,5 @@
 cd resourcepack
 zip -qr /tmp/pack.zip *
 cd ..
-rsync /tmp/pack.zip ron@bokkusu.ronthecookie.me:/var/lib/rtc-files/arc86pack.zip
+rsync /tmp/pack.zip bokkusu:/var/lib/rtc-files/arc86pack.zip
 sha1sum /tmp/pack.zip > /tmp/pack.zip.sha1
-# rsync /tmp/pack.zip.sha1 vps:compose/data/cdn/arc86pack.sha1.txt
